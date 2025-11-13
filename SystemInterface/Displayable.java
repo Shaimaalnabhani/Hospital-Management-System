409a358e12cd36588be7cf229e6c2baa6639fddc
Shaimaalnabhani <shaimaalnabhani@gmail.com>
2025-11-13 14:02:07 +0400

    Modifying Displayable

--- SystemInterface/Displayable.java
+++ SystemInterface/Displayable.java
@@ -2,7 +2,11 @@ package SystemInterface;
 
 public interface Displayable {
 
-    void displayInfo();
+    String displayInfo(String str);
+
+    String displayInfo();
+
+    String displaySummary(String str);
 
     void displaySummary();
 }
